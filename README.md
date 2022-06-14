8bc751b508ea4c6a8db9c33688705ca2d23667ce
maxslimb <kpatel3032@gmail.com>
2022-06-14 15:48:24 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# First NFT Project
+
+This project uses a basic Hardhat and Solidity to create an NFT minter collection!
---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.1;
+import "hardhat/console.sol";
+
+contract MyEpicNFT{
+    constructor() {
+        console.log("This is my NFT contract.");
+    }
+}
